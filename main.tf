97cadaf9a4067fbebbea4a30f3841a52a68f90a9
Kevin Zeidler <kzeidler@8thlight.com>
2023-08-03 16:51:41 -0700

    remove unused ubuntu instance

--- main.tf
+++ main.tf
@@ -1,45 +1,12 @@
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-provider "aws" {
-  region  = "us-west-2"
-  profile = "personal"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
-}
-
 resource "aws_sqs_queue" "my_queue" {
   name = "my-queue"
 }
 
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
-
   assume_role_policy = jsonencode({
     Statement = [
       {
@@ -54,6 +21,13 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
+provider "aws" {
+  region  = "us-west-2"
+  profile = "personal"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_sqs_policy" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
@@ -65,11 +39,21 @@ resource "aws_iam_role_policy_attachment" "lambda_logs_policy" {
 }
 
 resource "aws_lambda_function" "my_lambda" {
-  function_name = "my_lambda_function"
-  handler       = "main.lambda_handler" # Name of the file and the function
-  runtime       = "python3.8"
-  role          = aws_iam_role.lambda_role.arn
-
+  source_code_hash = <<EOT
+filebase64sha256("lambda_function_payload.zip")
+EOT
+  runtime          = "python3.8"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "main.lambda_handler"
+  function_name    = "my_lambda_function"
   filename         = "lambda_function_payload.zip"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
 }
+
+resource "aws_lambda_event_source_mapping" "sqs_lambda_trigger" {
+  function_name    = aws_lambda_function.my_lambda.function_name
+  event_source_arn = aws_sqs_queue.my_queue.arn
+  enabled          = true
+  batch_size       = 5
+}
+
+
