de1024a43f1b4b9c7480ba3bd61cbb157769e73c
Kevin Zeidler <kzeidler@8thlight.com>
2023-08-03 16:04:14 -0700

    modified original config to create a lambda function and SQS instance, as well as a designated IAM role for each

--- main.tf
+++ main.tf
@@ -32,3 +32,44 @@ resource "aws_instance" "ubuntu" {
     Name = var.instance_name
   }
 }
+
+resource "aws_sqs_queue" "my_queue" {
+  name = "my-queue"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ],
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_sqs_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "my_lambda_function"
+  handler       = "main.lambda_handler" # Name of the file and the function
+  runtime       = "python3.8"
+  role          = aws_iam_role.lambda_role.arn
+
+  filename         = "lambda_function_payload.zip"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+}
